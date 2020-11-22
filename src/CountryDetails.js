82cc8c79757a4a5d714944466598d21bde37ad20
Angie Carolina <33725657+angiecarojas@users.noreply.github.com>
2020-11-22 18:51:32 -0500

    Country Details funcionando

--- src/CountryDetails.js
+++ src/CountryDetails.js
@@ -25,6 +25,39 @@ class CountryDetails extends React.Component {
     };
   }
 
+  componentDidMount() {
+    Promise.all([
+      fetch(
+        `https://restcountries.eu/rest/v2/name/${this.props.name}?fullText=true`
+      ),
+      fetch(`https://restcountries.eu/rest/v2/all`),
+    ])
+      .then(([res1, res2]) => {
+        return Promise.all([res1.json(), res2.json()]);
+      })
+      .then(([res1, res2]) => {
+        this.setState({
+          details: res1,
+          countries: res2,
+        });
+        this.borderCountry(res1[0].borders);
+      });
+  }
+
+  borderCountry = (borderCodes) => {
+    const { countries } = this.state;
+    let borderNames = [];
+    for (let i = 0; i < borderCodes.length; i++) {
+      const borderCode = borderCodes[i];
+      for (let i = 0; i < countries.length; i++) {
+        const item = countries[i];
+        if (item.alpha3Code === borderCode) {
+          borderNames.push(item.name);
+        }
+      }
+    }
+    this.setState({ borderCountries: borderNames });
+  };
 
   render() {
     const { details } = this.state;
@@ -40,7 +73,78 @@ class CountryDetails extends React.Component {
             <i className="fa fa-moon-o mode-toggler" aria-hidden="true"></i>{" "}
             Dark Mode
           </p>
-        </div>        
+        </div>
+
+        <div className="country">
+          <div className="back">
+            <Link to="/">
+              <button className="back-button">
+                <i
+                  className="fa fa-long-arrow-left arrow-left"
+                  aria-hidden="true"
+                ></i>
+                Back
+              </button>
+            </Link>
+          </div>
+
+          <div className="country-body">
+            <img
+              className="country-flag"
+              src={details[0].flag}
+              alt={`${details[0].name} flag`}
+            />
+
+            <div className="country-bottom">
+              <div className="country-description-1">
+                <h2>{details[0].name}</h2>
+                <div>
+                  <p>
+                    <span className="font-bold"> Native Name: </span>
+                    {details[0].nativeName}
+                  </p>
+                  <p>
+                    <span className="font-bold"> Population: </span>
+                    {details[0].population.toLocaleString("en-US")}
+                  </p>
+                  <p>
+                    <span className="font-bold">Region: </span>
+                    {details[0].region}
+                  </p>
+                  <p>
+                    <span className="font-bold">Sub Region: </span>
+                    {details[0].subregion}
+                  </p>
+                  <p>
+                    <span className="font-bold">Capital: </span>
+                    {details[0].capital}
+                  </p>
+                </div>
+              </div>
+
+              <div className="country-description-2">
+                <p>
+                  <span className="font-bold">Top Level Domain: </span>
+                  {details[0].topLevelDomain}
+                </p>
+                <p>
+                  <span className="font-bold">Currencies: </span>
+                  {details[0].currencies[0].name}
+                </p>
+                <Languages name={details[0].languages} />
+              </div>
+
+              <div className="border-country">
+                <span className="font-bold">Border Country: </span>
+                <div className="border-group">
+                  {this.state.borderCountries.map((border) => (
+                    <button className="border-name">{border}</button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
